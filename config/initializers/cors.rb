806719fe747f68a9d359cb59d8e5af2afb7cb180
Ben Karst <ben.karst@gmail.com>
2022-08-08 23:37:10 -1000

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins 'localhost:3000', '127.0.0.1:3000', 'https://blackhole-staging.vercel.app/'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put]
     end
   end
